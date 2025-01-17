beef60a6cb12f4a5cda146834c9b4ac501bc6737
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2025-01-17 23:15:34 +0600

    Update web app with latest changes

---
+++ JS/main.js
@@ -0,0 +1,90 @@
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+// JavaScript for rotating roles dynamically
+$(document).ready(function () {
+    const roles = ["Software Developer","Passionate Researcher", "UI/UX Designer", "Content Creator", "Web Developer"];
+    let roleIndex = 0;
+
+    function changeRole() {
+        const roleElement = $("#dynamic-role");
+        if (roleElement.length) {
+            roleElement.text(roles[roleIndex]);
+            roleIndex = (roleIndex + 1) % roles.length; // Cycle through roles
+        }
+    }
+
+    // Change role every 2 seconds
+    setInterval(changeRole, 1000);
+});
+
+
+
+
+$(document).ready(function () {
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
+
+
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+});
